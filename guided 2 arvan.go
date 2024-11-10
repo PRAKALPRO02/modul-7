57ce1bdbdc337ba39664973ec8cb5bf977206842
ArvanMurbiyanto10 <2311102074@ittelkom-pwt.ac.id>
2024-11-11 03:09:39 +0700

    Add files via upload

---
+++ guided 2 arvan.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	mahasiswa := map[string]string{
+		"20231001": "Andi",
+		"20231002": "Budi",
+		"20231003": "Cici",
+	}
+
+	mahasiswa["20231004"] = "Dedi"
+
+	fmt.Println("Daftar Mahasiswa:")
+	fmt.Println("NIM\t\tNama")
+	fmt.Println("-------------------------")
+	for nim, nama := range mahasiswa {
+		fmt.Printf("%s\t%s\n", nim, nama)
+	}
+
+	nim := "20231002"
+	fmt.Println("\nNama Mahasiswa dengan NIM", nim, "adalah", mahasiswa[nim])
+
+	delete(mahasiswa, "20231003")
+
+	fmt.Println("\nDaftar Mahasiswa setelah dihapus:")
+	fmt.Println("NIM\t\tNama")
+	fmt.Println("-------------------------")
+	for nim, nama := range mahasiswa {
+		fmt.Printf("%s\t%s\n", nim, nama)
+	}
+}
