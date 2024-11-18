dcfedb292b88d5956f3a4ff6ea7b2d54e90fa95b
2311102176_Destia Ananda Putra <162514636+DestiaAnandaPutra@users.noreply.github.com>
2024-11-18 19:19:01 +0700

    Add files via upload

---
+++ Destia Ananda Putra_2311102176_Modul 7/Unguided/Unguided4.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+const NMAX int = 127
+
+type tabel [NMAX]rune
+
+func isiArray(t *tabel, n *int) {
+	var char rune
+	fmt.Println("Masukkan karakter satu per satu (akhiri dengan titik '.'): ")
+	for *n = 0; *n < NMAX; *n++ {
+		fmt.Scanf("%c", &char)
+		if char == '.' {
+			break
+		}
+		t[*n] = char
+	}
+}
+
+func cetakArray(t tabel, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Printf("%c", t[i])
+	}
+	fmt.Println()
+}
+
+func balikanArray(t *tabel, n int) {
+	for i := 0; i < n/2; i++ {
+		t[i], t[n-i-1] = t[n-i-1], t[i]
+	}
+}
+
+func palindrom(t tabel, n int) bool {
+	for i := 0; i < n/2; i++ {
+		if t[i] != t[n-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var tab tabel
+	var m int
+
+	isiArray(&tab, &m)
+
+	fmt.Print("Teks asli: ")
+	cetakArray(tab, m)
+
+	balikanArray(&tab, m)
+
+	fmt.Print("Teks setelah dibalik: ")
+	cetakArray(tab, m)
+
+	if palindrom(tab, m) {
+		fmt.Println("Apakah palindrom?  true")
+	} else {
+		fmt.Println("Apakah palindrom?  false")
+	}
+}
